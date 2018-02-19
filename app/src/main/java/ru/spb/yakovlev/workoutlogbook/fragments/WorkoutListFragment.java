736ad1af2059e8f143e9838631ccd0943fd73679
AlekseyYakovlev <aleksey@yakovlev.spb.ru>
2018-02-19 19:21:43 +0300

    Added some fields filling + menu

--- app/src/main/java/ru/spb/yakovlev/workoutlogbook/fragments/WorkoutListFragment.java
+++ app/src/main/java/ru/spb/yakovlev/workoutlogbook/fragments/WorkoutListFragment.java
@@ -1,16 +1,23 @@
 package ru.spb.yakovlev.workoutlogbook.fragments;
 
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -79,6 +86,45 @@ public class WorkoutListFragment extends Fragment {
             Workout workout = workouts.get(position);
             holder.titleText.setText(workout.getTitle());
             holder.descriptionTextView.setText(workout.getDescription());
+            holder.lastRecordReps.setText(String.valueOf(workout.getLastRecordReps()));
+            holder.lastRecordDate.setText(workout.getFormattedDate());
+
+
+            holder.menuIcon.setOnClickListener(v -> {
+                PopupMenu popupMenu = new PopupMenu(context, v);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_detail, popupMenu.getMenu());
+                popupMenu.show();
+
+                popupMenu.setOnMenuItemClickListener(menuItem -> {
+                    switch (menuItem.getItemId()) {
+                        case R.id.action_show: {
+                            Uri address = Uri.parse("http://sportwiki.to/%D0%9E%D1%82%D0%B6%D0%B8%D0%BC%D0%B0%D0%BD%D0%B8%D1%8F");
+                            Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+                            startActivity(openLinkIntent);
+                            return true;
+                        }
+                        case R.id.action_share: {
+                            String sbQuestion = String.format(getString(R.string.snack_bar_share_question), workout.getTitle());
+                            String messageText = String.format(getString(R.string.messageText), workout.getTitle(), workout.getLastRecordReps());
+                            String messageSubject = String.format(getString(R.string.messageSubject), workout.getTitle());
+                            Snackbar.make(getActivity().getCurrentFocus(), sbQuestion, Snackbar.LENGTH_LONG)
+                                    .setAction(getText(R.string.action_share), v1 -> {
+                                        Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                                        sendIntent.setType("text/plain");
+                                        sendIntent.putExtra("EXTRA_TEXT", messageText);
+                                        sendIntent.putExtra("EXTRA_SUBJECT", messageSubject);
+                                        startActivity(sendIntent);
+                                        Toast.makeText(context, messageText, Toast.LENGTH_SHORT).show();
+                                    })
+                                    .setActionTextColor(Color.BLUE)
+                                    .show();
+                            return true;
+                        }
+                        default:
+                            return false;
+                    }
+                });
+            });
             holder.cardView.setOnClickListener(v -> mCallback.onWorkoutSelected(position));
         }
 
@@ -93,12 +139,18 @@ public class WorkoutListFragment extends Fragment {
         CardView cardView;
         TextView titleText;
         TextView descriptionTextView;
+        TextView lastRecordReps;
+        TextView lastRecordDate;
+        ImageView menuIcon;
 
         public WorkoutViewHolder(View itemView) {
             super(itemView);
             cardView = itemView.findViewById(R.id.item_view_card_view);
             titleText = itemView.findViewById(R.id.item_view_title_text_view);
             descriptionTextView = itemView.findViewById(R.id.item_view_description_text_view);
+            lastRecordReps = itemView.findViewById(R.id.item_view_reps_text_view);
+            lastRecordDate = itemView.findViewById(R.id.item_view_record_date_text_view);
+            menuIcon = itemView.findViewById(R.id.item_view_context_menu);
         }
     }
 
